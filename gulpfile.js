7f3ad3657ca67425aa881d0547dab6e951a0a325
Iryna Kovalenko <ikovalenko@bsuir.by>
2020-01-20 13:31:34 +0200

    Оптимизация

--- gulpfile.js
+++ gulpfile.js
@@ -12,12 +12,14 @@ var csso = require("gulp-csso");
 var server = require("browser-sync").create();
 
 var imagemin = require("gulp-imagemin");
+var imgCompress  = require('imagemin-jpeg-recompress');
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -37,7 +39,12 @@ gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
-      imagemin.mozjpeg({quality: 85}),
+      imgCompress({
+        loops: 4,
+        min: 70,
+        max: 80,
+        quality: 'high'
+      }),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("build/img"));
@@ -63,9 +70,9 @@ gulp.task("html", function () {
     .pipe(posthtml([
       include()
     ]))
+    .pipe(htmlmin())
     .pipe(gulp.dest("build"));
 });
-
 
 gulp.task("copy", function () {
   return gulp.src([
@@ -100,7 +107,7 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/less/**/*.less", gulp.series("css", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
